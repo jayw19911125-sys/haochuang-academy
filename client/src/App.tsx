53c72c60408bc9b71ab99160e96abe11188460a5
jayw19911125@gmail.com <jayw19911125@gmail.com>
2026-07-19 05:25:01 +0800

    Build corrected Academy V2.1 planner system

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,12 +6,13 @@ import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
 import AdminDashboard from "./pages/AdminDashboard";
+import AcademyV2Corrected from "./pages/AcademyV2Corrected";
 
 function Router() {
-  // make sure to consider if you need authentication for certain routes
   return (
     <Switch>
       <Route path={"/"} component={Home} />
+      <Route path={"/v2"} component={AcademyV2Corrected} />
       <Route path={"/admin"} component={AdminDashboard} />
       <Route path={"/404"} component={NotFound} />
       <Route component={NotFound} />
